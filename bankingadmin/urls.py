d3ea03ee8f88c363dc3b44b5c92031f824287f98
Harshil <harshil58558@gmail.com>
2024-09-20 10:43:56 +0530

    updated admin and added admin temp

--- bankingadmin/urls.py
+++ bankingadmin/urls.py
@@ -8,6 +8,17 @@ urlpatterns = [
     path("viewaccountadmin",views.viewaccountadmin.as_view(),name='viewaccountadmin'),
 
     
+     path("accountcheck",views.accountcheck,name='accountcheck'),
+
+    path("cardadm", views.cardadm,name="cardadm"),
+    path("carddel/", views.carddel,name="carddel"),
+
+    path("takeloan",views.takeloan,name='takeloan'),
+
+
+    path("lifeinsurancecheck",views.lifeinsurancecheck,name='lifeinsurancecheck'),
+    path("healthinsurancecheck",views.healthinsurancecheck,name='healthinsurancecheck'),
+    path("vehicleinsurancecheck",views.vehicleinsurancecheck,name='vehicleinsurancecheck'),
 
 
 ]
